e0691ff265fd9ecd94749e61e8d3ce84c69b3171
sahilkumardev <sahilkumar710@outlook.com>
2026-03-30 11:29:46 +0530

    feat(auth): integrate better-auth with PostgreSQL and Prisma
    - Added authentication routes in `app/api/auth/[...all]/route.ts`.
    - Created an auth client in `lib/auth-client.ts`.
    - Implemented authentication logic in `lib/auth.ts` using better-auth and Prisma adapter.
    - Configured Prisma in `prisma.config.ts` with PostgreSQL datasource.
    - Defined Prisma schema in `prisma/schema.prisma` with User, Session, Account, and Verification models.

---
+++ lib/auth.ts
@@ -0,0 +1,21 @@
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+import { PrismaClient } from "@/app/generated/prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+
+const prisma = new PrismaClient({
+  adapter: new PrismaPg({ connectionString: process.env.DATABASE_URL }),
+});
+
+export const auth = betterAuth({
+  database: prismaAdapter(prisma, {
+    provider: "postgresql",
+  }),
+
+  socialProviders: {
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    },
+  },
+});
