e8ff3083538d50ff0ee58f0e448a4102153aeea2
sahilkumardev <sahilkumar710@outlook.com>
2026-04-10 21:10:29 +0530

    feat: refactor useNetwork hook to utilize useSyncExternalStore for network status

--- hooks/use-network.ts
+++ hooks/use-network.ts
@@ -3,22 +3,21 @@
 import * as React from "react";
 
 export function useNetwork() {
-  const [isOnline, setNetwork] = React.useState<boolean>(true);
-
-  React.useEffect(() => {
-    if (typeof window === "undefined") return;
-
-    const updateNetworkStatus = () => setNetwork(window.navigator.onLine);
-
-    updateNetworkStatus();
-    window.addEventListener("offline", updateNetworkStatus);
-    window.addEventListener("online", updateNetworkStatus);
-
-    return () => {
-      window.removeEventListener("offline", updateNetworkStatus);
-      window.removeEventListener("online", updateNetworkStatus);
-    };
-  }, []);
+  const isOnline = React.useSyncExternalStore(
+    subscribe,
+    () => (typeof navigator !== "undefined" ? navigator.onLine : true),
+    () => true,
+  );
 
   return isOnline;
+}
+
+function subscribe(callback: () => void) {
+  window.addEventListener("offline", callback);
+  window.addEventListener("online", callback);
+
+  return () => {
+    window.removeEventListener("offline", callback);
+    window.removeEventListener("online", callback);
+  };
 }
